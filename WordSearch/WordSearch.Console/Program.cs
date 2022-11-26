dc331f100f2df0f401ab28a1ece38c7193133248
Collins Mphahlele <Collins.Mphahlele@eoh.com>
2022-11-26 12:51:13 +0200

    word search completed

--- WordSearch/WordSearch.Console/Program.cs
+++ WordSearch/WordSearch.Console/Program.cs
@@ -12,54 +12,63 @@ class Program
         IWordIteratorService interator = new WordIteratorService();
         var grid = gridBuilder.Build();
 
-        Console.WriteLine("Enter file path: ");
+        Console.WriteLine("******************Word Search***************");
+        Console.WriteLine("********************************************");
+        Console.Write("Enter file path or press enter to exit: ");
         textFile = Console.ReadLine();
 
-        if (File.Exists(textFile))
+        if (!string.IsNullOrEmpty(textFile) && !string.IsNullOrWhiteSpace(textFile))
         {
-            using (StreamReader file = new StreamReader(textFile))
+            if (File.Exists(textFile))
             {
-                string word;
-
-                while ((word = file.ReadLine()) != null)
+                using (StreamReader file = new StreamReader(textFile))
                 {
-                    word = word.ToUpper();
-                    var results = interator.Search(ref grid, width, height, ref word, 0);
+                    string word;
 
-                    if (!results)
+                    while ((word = file.ReadLine()) != null)
                     {
-                        interator.ClearSearch(ref grid);
-                        Console.WriteLine(@"{0} - Not Found", word);
-                    }
-                    else
-                    {
-                        Console.WriteLine(@"{0} - Found", word);
-                    }
-                    Console.WriteLine();
+                        word = word.ToUpper();
+                        var results = interator.Search(ref grid, width, height, ref word, 0);
 
-                    //Print Grid
-                    for (int x = 0; x < width; x++)
-                    {
-                        for (int y = 0; y < height; y++)
+                        if (!results)
                         {
-                            if (grid[x, y, 1] == '*')
-                            {
-                                Console.ForegroundColor = ConsoleColor.Yellow;
-                                Console.Write(grid[x, y, 0]);
-                            }
-                            else
-                            {
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write(grid[x, y, 0]);
-                            }
+                            interator.ClearSearch(ref grid);
+                            Console.WriteLine(@"{0} - Not Found", word);
+                        }
+                        else
+                        {
+                            Console.WriteLine(@"{0} - Found", word);
                         }
                         Console.WriteLine();
-                    }
 
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("*****************************************************");
-                    interator.ClearSearch(ref grid);
+                        //Print Grid
+                        for (int x = 0; x < width; x++)
+                        {
+                            for (int y = 0; y < height; y++)
+                            {
+                                if (grid[x, y, 1] == '*')
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.Write(grid[x, y, 0]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write(grid[x, y, 0]);
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("*****************************************************");
+                        interator.ClearSearch(ref grid);
+                    }
                 }
+            }
+            else
+            {
+                Console.WriteLine("File does not exists.");
             }
         }
     }
